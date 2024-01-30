1cf4f0103b4c89d694a71b3a458aaac7d6a82b62
Amarbat <Amarbat@172.16.152.249>
2024-01-30 14:37:38 +0800

    Fifth commit

--- Buteelt/src/lab/lab1.java
+++ Buteelt/src/lab/lab1.java
@@ -4,6 +4,9 @@ public class lab1 {
 	public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("snu snu snu");
+        System.out.println("2:37");
+
+        
 
     }
 }
