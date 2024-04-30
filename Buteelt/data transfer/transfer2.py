ebd8e0672703a0089993491460d1a262015a8cce
Yunoqnn <yuriosama04@gmail.com>
2024-04-30 17:33:58 +0800

    ver 2

--- Buteelt/data transfer/transfer2.py
+++ Buteelt/data transfer/transfer2.py
@@ -5,12 +5,12 @@ from pymongo import MongoClient
 server = 'DESKTOP-M86GRUG/SQLEXPRESS'
 database = 'ULAANBAATAR'
 mongo_uri = 'mongodb://localhost:27017/'
-mongo_db = 'company' 
+mongo_db = 'darhan' 
 mongo_collection = 'employee'  
 
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
 
-query = "SELECT * FROM EMPLOYEES WHERE ХЭЛТЭС LIKE '%IT%'"
+query = "SELECT * FROM EMPLOYEES"
 cursor = conn.cursor()
 cursor.execute(query)
 
@@ -28,4 +28,22 @@ data = df.to_dict(orient='records')
 
 collection.insert_many(data)
 
+print("Verifying data in MongoDB...")
+mongo_data = list(collection.find())
+
+if len(mongo_data) > 0:
+    for document in mongo_data:
+        print(document)
+    
+    it_data = [doc for doc in mongo_data if 'IT' in doc.get('ХЭЛТЭС', '')]
+    for document in it_data:
+        print(document)
+    other_data_ids = [doc['_id'] for doc in mongo_data if 'IT' not in doc.get('ХЭЛТЭС', '')]
+    result = collection.delete_many({'_id': {'$in': other_data_ids}})
+    
+else:
+    print("No data found in MongoDB. Data transfer might have failed.")
+
 client.close()
+
+print("Process completed.")
